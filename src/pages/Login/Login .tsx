833a64e1c9477345b915380fca60fb58e1c951c1
andres0818 <c.andres.perez.h@gmail.com>
2023-01-29 16:10:04 -0500

    login restructuring

--- src/pages/Login/Login .tsx
+++ src/pages/Login/Login .tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { cajero, google } from './img'
+import React, { useState } from 'react'
+import { app } from '../../fb'
+import { cajero, admin,usuario } from './img'
+import { NavigateFunction, useNavigate } from 'react-router-dom'
+
 import './Login.scss'
 
-const Login = () => {
+const Login: Function = () => {
+
+  const [modal,setModal] =useState<boolean>(false)
+
+  const navigate: NavigateFunction = useNavigate()
+
+  const loginAdmin: Function = () => {
+
+  }
+
   return (
     <div className='login'>
       <img className='login__cajero' src={cajero} alt="" />
-      <img className='login__google' src={google} alt="" />
+      <img onClick={() => loginAdmin()} className='login__admin' src={admin} alt="" />
+      <img onClick={() => loginAdmin()} className='login__usuario' src={usuario} alt="" />
       <h2 className='login__titulo'>Bienvenido</h2>
       <h3 className='login__subtitulo'>Inicie sesion</h3>
     </div>
--- src/pages/Login/img/index.js
+++ src/pages/Login/img/index.js
@@ -1,5 +1,7 @@
 import cajero from "./cajero-automatico.png"
 import google from "./google.png"
+import admin from "./administracion.png"
+import usuario from "./hombre.png"
 
 
-export {cajero, google} 
+export {cajero, google, admin, usuario}
